b0257030d1943064eaed39cec760250c8157a0df
Hyojoon Park <hpar9758@naver.com>
2025-02-03 19:03:09 +1100

    chore(coupon-service): Add base classes for Kafka producer and consumer
    - Create CouponProducer as a placeholder for Kafka message production
    - Create CouponConsumer as a placeholder for Kafka message consumption
    - Create CouponService for future business logic implementation

---
+++ coupon-service/src/main/java/com/example/couponservice/service/v3/CouponProducer.java
@@ -0,0 +1,9 @@
+package com.example.couponservice.service.v3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CouponProducer {
+}
---
+++ coupon-service/src/main/java/com/example/couponservice/service/v3/CouponService.java
@@ -0,0 +1,12 @@
+package com.example.couponservice.service.v3;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service("couponServiceV3")
+@RequiredArgsConstructor
+public class CouponService {
+
+}
